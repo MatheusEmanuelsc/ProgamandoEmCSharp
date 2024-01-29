b26652eaa4bbdfef170341f5d596acb234bb024a
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-01-29 13:22:57 -0300

    Projeto 1 finalizado

---
+++ basics/EntradaESaidaDados.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgamandoEmCSharp.basics
+{
+    public class EntradaESaidaDados
+    {
+        // Saida de dados
+        Console.Write("seilar");//escreve algo
+        Console.WriteLine("Hello, World!");//escreve algo e pula uma linha
+
+        // Entrada de dados
+        Console.Read();//let um item
+        Console.ReadLine();// ler uma linha
+
+        Console.clear();//limpa a tela
+    }
+}
