557bf2b3c5706cbf68d44850b5357a1d5747d3c6
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-02-11 21:07:59 -0300

    Poo parte 1

---
+++ poo/Poo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Poo.Classes;
+
+Console.WriteLine("Hello, World!");
+
+
+
+using (PagamentoCartaoCredito pag = new(DateTime.Now))
+{
+    //aqui vc botaria todas operações com o metodo
+}
