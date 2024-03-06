443ff1104633179a65ce6bb33f69cdf217122996
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-06 11:49:28 -0300

    variaveis

---
+++ Fundamentos/02-Variaveis/Program.cs
@@ -0,0 +1,29 @@
+﻿// variaveis
+
+/*
+    1 tipo de dadps da variavel
+    2 o nome da variavel
+    3 Atribuição de um valor(opcional) usando sinal =(igual)
+
+*/
+
+string issoEUmaVariavel="E uma variavel";
+
+// Outro modo de declara uma variavel
+
+var issoEUmaVariavel2="variavel2";
+// Continua sendo tipado porem ele descobre o tipo da variavel no tempo de exe
+
+// Constante
+
+/* Possui todas caracterisca acima da variavel 
+
+porem uma vez que o valor seja definido ele não podera ser mudado
+
+*/
+
+const string issoEUmaConstante="Constante";
+
+Console.WriteLine(issoEUmaVariavel);
+Console.WriteLine(issoEUmaVariavel2);
+Console.WriteLine(issoEUmaConstante);
