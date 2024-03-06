944d649487d270ae66811c6851c6b5e1d14d933f
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-06 11:05:17 -0300

    Reformulacao

---
+++ Fundamentos/01-LeituraEscritaDados/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+// escreve uma linha
+Console.Write("Ola Mundo");
+
+// Escreve  e pula uma linha
+Console.WriteLine("Hello, World!");
+
+// ler o proximo carcterer
+Console.Read();
+
+// ler uma linha
+Console.ReadLine();
+//Obs ele recebe o valor em string caso precise de outro valor tipo int ou double tem que fazer o parse
+
+// ler a tecla que foi digitada
+Console.ReadKey();
