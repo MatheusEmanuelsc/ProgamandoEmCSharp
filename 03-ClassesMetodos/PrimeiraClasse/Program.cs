12619eed5ae0bbb0e8c4bd7dad47c47d9f2e137c
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-14 17:07:32 -0300

    Poo inicio

---
+++ 03-ClassesMetodos/PrimeiraClasse/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+// Com a chegada do net 6 agora e possviel criar classes no program basicamente a execução em cima e as classes embaixo
+
+
+// Criando o objeto
+Pessoa p1 = new Pessoa();
+p1.nome="jao";
+p1.idade=26;
+p1.sexo="masc";
+
+Console.WriteLine(p1);
+
+
+
+// Criando a classe
+class Pessoa
+{   
+    // propriedades
+    public string? nome { get; set; }
+    public int idade { get; set; }
+    public string? sexo { get; set; }
+}
