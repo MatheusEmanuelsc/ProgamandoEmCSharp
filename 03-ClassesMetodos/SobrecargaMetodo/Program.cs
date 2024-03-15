9adcd2604e3c16be0ba8ff8c700fd8338e9992b2
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-15 18:47:14 -0300

    varios metodos

---
+++ 03-ClassesMetodos/SobrecargaMetodo/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Seilar seilar = new Seilar();
+
+seilar.digitaAlgo();
+seilar.digitaAlgo("seilar tanto faz");
+
+
+class Seilar
+{
+    
+    // vc pode ter metodos com mesmo nome desde que os parametros seja diferente como no exemplo abaixo
+
+    public void digitaAlgo(){
+        Console.WriteLine("Algo");
+    }
+
+    public void digitaAlgo(string algo){
+        Console.WriteLine(algo);
+    }
+}
