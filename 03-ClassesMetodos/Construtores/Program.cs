213480200a68a987014fad5793b028f68f150448
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-15 17:30:31 -0300

    metodos

---
+++ 03-ClassesMetodos/Construtores/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+class Pessoa
+{
+    public string? nome { get; set; }
+    public int idade { get; set; }
+    public string? sexo { get; set; }
+
+    // construtor padrão
+    public Pessoa()
+    {
+        
+    }
+
+    // construtor com parametros
+    public Pessoa(string nome,int idade)
+    {
+        this.nome =nome;
+        this.idade =nome;
+        
+    } 
+
+
+    //classes  podem ter mais de um construtor  com parametros diferentes
+    
+
+
+    public Pessoa(string sexo):this(nome,idade)
+    {
+        this.sexo =sexo;
+    }//construtor chamando outro construtor
+}
