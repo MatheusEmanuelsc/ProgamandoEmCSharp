1038bb7cb2e5a0d21d1e3da173c10d2e8df17eb9
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-16 19:38:02 -0300

    argumentos

---
+++ 03-ClassesMetodos/06-ArgumentosNomeados/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("## Argumentos nomeados ##\n");
+
+Email email = new();
+
+email.Enviar("teste@email.com", "Reunião de Orçamento","Urgente" );
+
+//argumentos nomeados
+email.Enviar(titulo: "Urgente", destino: "teste@email.com", assunto: "Reunião Orçamento");
+email.Enviar(destino: "teste@email.com", assunto: "Reunião Orçamento", titulo: "Urgente");
+email.Enviar(assunto: "Reunião Orçamento", titulo: "Urgente", destino: "teste@email.com");
+
+// basicamente no momento de passamos os dados utilizamos o nomes dos argumentos assim independe da ordem que for passada os dados serão lidos da forma correta
+
+Console.ReadKey();
+
+public class Email
+{
+    public void Enviar(string destino, string titulo, string assunto)
+    {
+        Console.WriteLine($"\nPara {destino} - {titulo} \n Assunto: {assunto}");
+    }
+}
