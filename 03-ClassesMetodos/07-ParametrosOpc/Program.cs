4d85da91d0dbf4c12b98c42a28bafdecff1b0a11
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-16 19:48:36 -0300

    opc

---
+++ 03-ClassesMetodos/07-ParametrosOpc/Program.cs
@@ -0,0 +1,32 @@
+﻿﻿Console.WriteLine("## Parâmetros opcionais ##\n");
+
+Console.WriteLine("Informe o destino");
+var destino = Console.ReadLine();
+
+Console.WriteLine("Informe o titulo");
+var titulo = Console.ReadLine();
+
+Console.WriteLine("Informe o assunto");
+var assunto = Console.ReadLine();
+
+Email email = new();
+email.Enviar();
+email.Enviar(destino);
+email.Enviar(destino, titulo);
+email.Enviar(destino, titulo, assunto);
+
+//não posso fazer
+//email.Enviar(destino, , assunto);//causa um erro
+email.Enviar(destino, assunto: assunto);
+
+Console.ReadKey();
+
+public class Email
+{
+	//parametros opicionais são definidos sempre no final como no exmeplo abaixo
+	// para  fazelo opicionais basta adc um valor constante como no exemplo
+    public void Enviar(string destino = "Destino padrão", string titulo = "Titulo Padrão", string assunto = "Assunto Padrão")
+    {
+        Console.WriteLine($"\nPara {destino} - {titulo} \n Assunto: {assunto}");
+    }
+}
