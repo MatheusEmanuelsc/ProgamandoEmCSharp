8a6c64a3787511a07f73b52d5b70577dd515fdae
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-24 15:22:05 -0300

    propriedades

---
+++ 03-ClassesMetodos/08-MetodoEstatico/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// metodo static
+
+
+public static string seilar(){
+    Console.WritiLine("Pode ser");
+}
+
+/*
+    Os metodos estaticos  são metodos que não dependem de um objeto ou seja não dependem da criação de uma instancia de classe para serem acessados
+
+    os metodos estaticos pertecem a classe e não ao objeto
+
+    como o metodo estatico não possui ligação com um objeto ele não pode usar variavies de instancia que são variaveis do objeto
+
+*/
+
+/*
+    Existe tbm os contrutores staticos no qual so pode se ter 1 por classe ele não recebe parametros ele sera inicializado antes da classe ser inicializada e vc pode ter construtores normais tbm e sera executado apenas uma vez
+
+*/
---
+++ 03-ClassesMetodos/09-Propriedades/Program.cs
@@ -0,0 +1,13 @@
+﻿class Pessoa
+{
+    public string Nome { get; set; }
+    // nessa propriedade e feita por de baixo dos panos  o metodo está privado e vc acessar ele pelo get e set assim não quebrando um dos paradigmas
+
+    public int idade;
+    // nessa não e feito isso por isso quebrar e não e recomendando utiliza-la
+
+
+    public int num1 { get; set; }//permite leitura e escrita
+    public int num2 { get; }//apenas leitura
+    public int num3 {  set; }//apenas escrita
+}
