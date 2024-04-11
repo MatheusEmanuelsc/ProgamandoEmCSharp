677f1e0844cf996d0f42b3b66a123ee658fcb50c
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-04-11 15:03:25 -0300

    exercicio 3 inicio

---
+++ 14-Exercicios/03/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("---------------");
+Console.WriteLine("Questão 1: ");
+
+Console.WriteLine("Digite o primeiro numero:");
+var num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero:");
+var num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro numero:");
+var num3 = int.Parse(Console.ReadLine());
+
+var maior = 0;
+var numMaior = "";
+if (num1 >= num2)
+{
+    maior = num1;
+    numMaior = "primeiro numero";
+}else{
+    maior = num2;
+    numMaior = "segundo numero";
+}
+
+if (num3 > maior){
+    maior = num3;
+    numMaior = "terceiro numero";
+}
+
+Console.WriteLine($"O {numMaior}: {maior} é o maior  ");
+Console.WriteLine("---------------");
